f221d474734b17da5e5d995260ac22960a89bf26
QuenMBar <Qbeast360@gmail.com>
2021-02-22 17:26:26 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(mult) {
+    return (fare) => {
+        return fare * mult;
+    }
+}
+
+let fareDoubler = (fare) => createFareMultiplier(2)(fare);
+let fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(drivers, firstOrLastFun) {
+    return firstOrLastFun(drivers);
+}
